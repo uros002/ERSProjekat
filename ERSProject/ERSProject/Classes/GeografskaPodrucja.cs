b947b9d55a28fdf4084412175c2c2a5a1e803ba2
uros002 <urosgrahovac002@gmail.com>
2023-12-17 13:16:19 +0100

    Projekat napravljen uz napravljene pocetne klase
    Pravljenje projekta zajedno sa kosturom klasa GeografskoPodrucje, PrognoziranaPotrosnja,OstvarenaPotrosnja, Dodat i korisnicki interfejs prikazan preko switch case.

---
+++ ERSProject/ERSProject/Classes/GeografskaPodrucja.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERSProject.Classes
+{
+    public class GeografskaPodrucja
+    {
+        public int SifraPodrucja { get; set; }
+
+        public string NazivPodrucja { get; set; }
+
+        public string SirinaPodrucja { get; set; }
+
+        public GeografskaPodrucja(int sifra,string naziv,string sirina)
+        {
+            this.SifraPodrucja = sifra;
+            this.NazivPodrucja = naziv;
+            this.SirinaPodrucja = sirina;
+        }
+
+    }
+}
---
+++ ERSProject/ERSProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERSProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Odaberite opciju:");
+                Console.WriteLine("1 - Uvoz podataka");
+                Console.WriteLine("2 - Ispis podataka o ostvarenoj potrosnji");
+                Console.WriteLine("3 - Evidencija geografskih podrucja");
+                Console.WriteLine("X - Izlazak");
+                input = Console.ReadLine();
+
+
+                switch (input)
+                {
+                    case "1":
+
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                }
+
+            } while (!input.ToLower().Equals("x"));
+        }
+    }
+}

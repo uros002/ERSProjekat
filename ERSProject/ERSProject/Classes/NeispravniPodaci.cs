84eed06f84ae72d942e3514634a01cc9eaba26d5
natasajevremov1 <148207412+natasajevremov1@users.noreply.github.com>
2023-12-19 16:12:25 +0100

    Napravljena klasa sa podacima koji su potrebni za nevalidan fajl

---
+++ ERSProject/ERSProject/Classes/NeispravniPodaci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERSProject.Classes
+{
+    class NeispravniPodaci
+    {
+        public string imeFajla;
+        public string lokacija;
+        public int sat;
+        public int minuti;
+        public int sekunde;
+
+        public NeispravniPodaci(string filePath)
+        {
+            imeFajla = Path.GetFileName(filePath);
+            lokacija = filePath;
+            DateTime vreme = DateTime.Now;
+            sat = vreme.Hour;
+            minuti = vreme.Minute;
+            sekunde = vreme.Second;
+        }
+    }
+}
+

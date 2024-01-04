3d9398aab4e6eb2497a7f9f7ade6506b3cd7a51a
natasajevremov1 <148207412+natasajevremov1@users.noreply.github.com>
2024-01-04 13:09:32 +0100

    dodata klasa NeispravniPodaci
    pravljenje xml fajla za smestanje podataka koji nisu ispravni

--- ERSProject/ERSProject/XML FUNCTIONS/WriteReadXMLImplement.cs
+++ ERSProject/ERSProject/XML FUNCTIONS/WriteReadXMLImplement.cs
@@ -15,6 +15,103 @@ namespace ERSProject
         private SettingUpPaths setUpPaths = new SettingUpPaths();
         private SettingUpPaths setUpPathsWrite = new SettingUpPaths();
 
+        /*
+        private void LogInvalidFile(string document, XmlDocument xmlDoc)
+    {
+        NeispravniPodaci neispravniPodaci = new NeispravniPodaci(document);
+
+        // Evidentirajte neispravne podatke u novi XML fajl
+        XmlDocument invalidXmlDoc = new XmlDocument();
+        invalidXmlDoc.Load("C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\nevalidniPodaci.xml");
+
+        XmlNode invalidFileNode = invalidXmlDoc.CreateElement("InvalidFile");
+        invalidXmlDoc.DocumentElement.AppendChild(invalidFileNode);
+
+        XmlNode fileNameNode = invalidXmlDoc.CreateElement("FileName");
+        fileNameNode.InnerText = neispravniPodaci.imeFajla;
+        invalidFileNode.AppendChild(fileNameNode);
+
+        XmlNode locationNode = invalidXmlDoc.CreateElement("Location");
+        locationNode.InnerText = neispravniPodaci.lokacija;
+        invalidFileNode.AppendChild(locationNode);
+
+        XmlNode timeNode = invalidXmlDoc.CreateElement("Time");
+        timeNode.InnerText = $"{neispravniPodaci.sat}:{neispravniPodaci.minuti}:{neispravniPodaci.sekunde}";
+        invalidFileNode.AppendChild(timeNode);
+
+        invalidXmlDoc.Save("C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\nevalidniPodaci.xml");
+    }         
+         */
+
+
+        /*public void CheckAndLogInvalidFiles(string document)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load("C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\" + document);
+
+        if (!IsValid(xmlDoc))
+        {
+            LogInvalidFile(document, xmlDoc);
+        }
+    }
+         */
+
+
+        /*
+         private bool IsValid(XmlDocument xmlDoc)
+          {
+               XmlNodeList sat = xmlDoc.GetElementsByTagName("SAT");
+    XmlNodeList potrosnja = xmlDoc.GetElementsByTagName("LOAD");
+    XmlNodeList oblast = xmlDoc.GetElementsByTagName("OBLAST");
+
+    // Provera broja sati u danu
+    int brojSatiUDanu = 24;  // Možete prilagoditi ovo prema potrebi
+
+    if (sat.Count < brojSatiUDanu - 1 || sat.Count > brojSatiUDanu + 1)
+    {
+        return false;
+    }
+
+    // Provera formata i unosa za svaku oblast
+    Dictionary<string, int> oblasti = new Dictionary<string, int>();
+    
+    for (int i = 0; i < sat.Count; i++)
+    {
+        int satVrednost, potrosnjaVrednost;
+
+        if (!int.TryParse(sat[i].InnerText, out satVrednost) ||
+            !int.TryParse(potrosnja[i].InnerText, out potrosnjaVrednost))
+        {
+            return false;  // Ako format sati ili potrošnje nije ispravan
+        }
+
+        string oblastVrednost = oblast[i].InnerText;
+
+        // Provera unosa za svaku oblast
+        if (!oblasti.ContainsKey(oblastVrednost))
+        {
+            oblasti[oblastVrednost] = 1;
+        }
+        else
+        {
+            oblasti[oblastVrednost]++;
+        }
+    }
+
+    // Provera da li svaka oblast ima tačno onoliko redova koliko ima sati u danu
+    foreach (var pair in oblasti)
+    {
+        if (pair.Value != brojSatiUDanu)
+        {
+            return false;
+        }
+    }
+
+    return true;  // Ako sve provere prolaze
+   }
+          
+         */
+
         public int ReadFromXML(string document)
         {
             List<string>Paths  = setUpPaths.SettUpPathsRead();
@@ -23,8 +120,13 @@ namespace ERSProject
             {
                 XmlDocument xmlDoc = new XmlDocument();
                 //string path = "C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\";
-                 xmlDoc.Load("C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\" + path);
-               // xmlDoc.Load(path);
+                 //xmlDoc.Load("C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\" + path);
+
+                xmlDoc.Load("C:\\Users\\Win10\\Documents\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\" + path);
+                // xmlDoc.Load(path);
+
+                //CheckAndLogInvalidFiles(path);    N
+
                 XmlNodeList sat = xmlDoc.GetElementsByTagName("SAT");
                 XmlNodeList potrosnja = xmlDoc.GetElementsByTagName("LOAD");
                 XmlNodeList oblast = xmlDoc.GetElementsByTagName("OBLAST");
@@ -98,8 +200,9 @@ namespace ERSProject
                 pathWrite = "prog_potrosnja.xml";
             }
                 //string path = "C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\prog_potrosnja.xml";
-                xmldoc.Load("C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\" + pathWrite);
-                XmlNode Stavka = xmldoc.CreateElement("Stavka");
+            // xmldoc.Load("C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\" + pathWrite);
+               xmldoc.Load("C:\\Users\\Win10\\Documents\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\" + pathWrite);
+            XmlNode Stavka = xmldoc.CreateElement("Stavka");
 
                 XmlNode Sat = xmldoc.CreateElement("Sat");
                 Sat.InnerText = sat.ToString();
@@ -115,8 +218,9 @@ namespace ERSProject
 
                 xmldoc.DocumentElement.AppendChild(Stavka);
 
-                xmldoc.Save("C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\" + pathWrite);
-            
+              //  xmldoc.Save("C:\\Users\\User\\OneDrive\\Dokumenti\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\" + pathWrite);
+              xmldoc.Save("C:\\Users\\Win10\\Documents\\GitHub\\ERSProjekat\\ERSProject\\ERSProject\\Source\\" + pathWrite);
+
         }
     }
 }
